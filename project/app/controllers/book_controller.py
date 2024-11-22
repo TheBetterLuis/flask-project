077e6eb270a502c366d0d5f8d0a4248cb4bd83e1
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-21 23:45:18 -0400

    books done

--- project/app/controllers/book_controller.py
+++ project/app/controllers/book_controller.py
@@ -76,3 +76,77 @@ def create_book(data):
         print(f"Error creating book: {str(e)}")
         return {"message":"Error creating book"}
 
+
+def update_user2(id, data):
+  # Obtener los datos enviados en la peticion
+  if not data:
+    return {"mensaje":"Debe enviarse los datos"}
+  
+  cursor = mysql.connection.cursor()
+  
+  update_query = "UPDATE users SET "
+  update_data = []
+  
+  for campo, valor in data.items():
+    if campo in ['nombre','apellido']:
+      update_query += f"{campo} = %s, " # UPDATE users SET nombre = %s, 
+      update_data.append(valor) # ["elimelech", "attale"]
+      
+  if not update_data:
+    return {"mensaje":"Debe enviarse los datos"}
+  
+  # Vamos a eliminar la coma del final
+  update_query = update_query.rstrip(", ")
+  
+  # Agregamos la condicion
+  update_query += " WHERE id = %s" # UPDATE users SET nombre = %s WHERE id = %s
+  
+  update_data.append(id)
+  
+  # Ejecutamos la consulta
+  cursor.execute(update_query, tuple(update_data,))
+  
+  mysql.connection.commit()
+  cursor.close()
+  
+  return {"mensaje":"Usuario actualizado con exito"}
+
+#Patch
+def update_book(id,data):
+    if not data:
+        return {"message":"Must send new data"}
+
+    cursor = mysql.connection.cursor()
+    update_query = "UPDATE books SET "
+    update_data = []
+    for field, value in data.items():
+        if field in ['title','published_date','author_id','genre_id','editorial_id']:
+            update_query += f"{field} = %s, "
+            update_data.append(value)
+    if not update_data:
+        return {"message":"Must send data"}
+    update_query = update_query.rstrip(", ") 
+    update_query += " WHERE id = %s"
+    update_data.append(id)
+
+    try:
+        cursor.execute(update_query, tuple(update_data,))
+        mysql.connection.commit()
+        cursor.close()
+        return {"message":"book updated successfully."}
+    except Exception as e:
+        print(f"Error updating book: {str(e)}")
+        return {"message":"Error updating book"}
+
+#delete
+def delete_book(id):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('DELETE FROM books WHERE id = %s', (id,))
+        mysql.connection.commit()
+        cursor.close()
+        return {"message":"Book deleted successfully."}
+    except Exception as e:
+        print(f"Error deleting book: {str(e)}")
+        return {"message":"Error deleting book"}
+
--- project/app/routes/book_routes.py
+++ project/app/routes/book_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,jsonify,request
 
-from app.controllers.book_controller import get_books,get_book,create_book,get_all
+from app.controllers.book_controller import get_books,get_book,create_book,get_all,update_book,delete_book
 
 book = Blueprint('book',__name__)
 
@@ -24,4 +24,13 @@ def get_one_book(book_id):
 def add_book():
     return jsonify(create_book(request.json))
 
+#patch
+@book.route('/books/<book_id>',methods=['PATCH'])
+def patch_book(book_id):
+    return jsonify(update_book(book_id, request.json))
+
+#delete
+@book.route('/books/<book_id>',methods=['DELETE'])
+def erase_book(book_id):
+    return jsonify(delete_book(book_id))
 
