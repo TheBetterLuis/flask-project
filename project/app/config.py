86a718c5744bee5d85b532fd3ebf40854d72d177
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-22 13:59:27 -0400

    Update config.py

--- project/app/config.py
+++ project/app/config.py
@@ -1,5 +1,5 @@
 class Config:
     MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD= '1919'
-    MYSQL_DB= 'library'
+    MYSQL_USER = 'your_user'
+    MYSQL_PASSWORD= 'your_password'
+    MYSQL_DB= 'your_database'
