091fb5aa2a7a7b410b1a691d1255d74d9b18638c
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2024-11-22 00:42:32 -0400

    done? gonna do some testing

--- project/app/controllers/genre_controller.py
+++ project/app/controllers/genre_controller.py
@@ -12,6 +12,23 @@ def get_genres():
         print(f"Error fetching genres: {str(e)}")
         return {"error":"Failed to fetch genres"}
 
+def get_genre(id):
+    try:
+        cursor=mysql.connection.cursor()
+        cursor.execute('SELECT * FROM genres WHERE id = %s',(id,))
+        columns = [column[0] for column in cursor.description]
+        response = dict(zip(columns,cursor.fetchone()))
+        print(response)
+        cursor.close()
+
+        if response:
+            return response
+        else:
+            return {"message":"Can't find genre"}
+    except Exception as e:
+        print(f"Error fetching genre: {str(e)}")
+        return {"message":"Error fetching genre"}       
+
 
 #POST
 def create_genre(data):
@@ -29,4 +46,30 @@ def create_genre(data):
         print(f"Error creating genre: {str(e)}")
         return {"message":"Error creating genre"}
 
+#Patch
+def update_genre(id,data):
+    name = data.get('name')
 
+    if not name:
+        return {"message":"Must include new genre name"}
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('UPDATE genres SET name = %s WHERE id =%s', (name, id))
+        mysql.connection.commit()
+        cursor.close()
+        return {"message":"Genre updated successfully."}
+    except Exception as e:
+        print(f"Error updating author: {str(e)}")
+        return {"message":"Error updating genre"}
+
+#delete
+def delete_genre(id):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('DELETE FROM genres WHERE id = %s', (id,))
+        mysql.connection.commit()
+        cursor.close()
+        return {"message":"Genre deleted successfully."}
+    except Exception as e:
+        print(f"Error deleting genre: {str(e)}")
+        return {"message":"Error deleting genre"}
--- project/app/routes/genre_routes.py
+++ project/app/routes/genre_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,jsonify,request
 
-from app.controllers.genre_controller import get_genres,create_genre
+from app.controllers.genre_controller import get_genres,create_genre,get_genre,update_genre,delete_genre
 
 genre = Blueprint('genre',__name__)
 
@@ -9,7 +9,21 @@ genre = Blueprint('genre',__name__)
 def get_all_genres():
     return jsonify(get_genres())
 
+@genre.route('/genres/<genre_id>',methods=['GET'])
+def get_one_genre(genre_id):
+    return jsonify(get_genre(genre_id))
+
 #post
 @genre.route('/genres',methods=['POST'])
 def add_genre():
     return jsonify(create_genre(request.json))
+
+#patch
+@genre.route('/genres/<genre_id>',methods=['PATCH'])
+def patch_genre(genre_id):
+    return jsonify(update_genre(genre_id, request.json))
+
+#delete
+@genre.route('/genres/<genre_id>',methods=['DELETE'])
+def erase_genre(genre_id):
+    return jsonify(delete_genre(genre_id))
